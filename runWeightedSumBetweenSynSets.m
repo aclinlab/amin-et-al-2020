83403ec0fa37db0efbcdbeaaf758ffb8c1ae7795
Andrew Lin <andrewclin@gmail.com>
2020-07-25 22:37:12 +0100

    update calculation of weighed sums
    -fix calyx ROI
    -update KC classifications and ratio of k vs lambda for v1.1 hemibrain

--- runWeightedSumBetweenSynSets.m
+++ runWeightedSumBetweenSynSets.m
@@ -1,35 +1,38 @@
-electroDistConstants = [625, 1250, 1875, 2381, 5590, 11180, Inf];
-realDistConstants = electroDistConstants*5;
+electroDistConstants = [624, 1249, 1873, 2381, 5590, 11180, Inf];
+realDistConstants = electroDistConstants*5.0062;
 
 % This section contains examples of commands used to generae Fig 8I-L (but
 % not every condition)
 % Fig 8J, K
-numKCs = 1929;
+numKCs = length(apl.synSet(1).uniquePartners);
 
 resultAll = zeros(numKCs, numKCs, length(electroDistConstants));
 % This takes about 90'
 for i=1:length(electroDistConstants)
     resultAll(:,:,i) = apl.weightedSumBetweenSynSets(electroDistConstants(i), KCtoAPLEDtoAPLtoKC, [2 1],...
-        {{'CA(R)','PED(R)','gL(R)','aL(R)', 'bL(R)', 'a''L(R)', 'b''L(R)'}});
+        {apl.synSet(1).synROIskey}); %{'CA(R)','PED(R)','gL(R)','aL(R)', 'bL(R)', 'a''L(R)', 'b''L(R)'}
 end
 save(strcat('results_allKCs_',datestr(now,'yyyymmddTHHMMSS'),'.mat'),'resultAll');
 
 % the following script is used to sort KCs by subtype:
-[resultsAllreordered, totalPerm1250] = apl.reorderKCs(resultAll(:,:,2)); % use 1250
+[resultsAllreordered, totalPerm1250] = apl.reorderKCsv11(resultAll(:,:,2)); % use 1250
 % save the order of KCs from 50 um, all KCs (Fig 8G)
 
 % Fig 8G
-writeTiffParulaWithRange(resultAll(totalPerm1250,totalPerm1250,2), 'allKCs.tiff',[0 500]);
+writeTiffParulaWithRange(resultAll(totalPerm1250,totalPerm1250,2), strcat('allKCs-',datestr(now,'yyyymmddTHHMM'),'.tiff'),[0 500]);
 % Fig 8H
-writeTiffParulaWithRange(resultAll(totalPerm1250,totalPerm1250,7), 'allKCsNoDecay.tiff',[0 8000]);
+writeTiffParulaWithRange(resultAll(totalPerm1250,totalPerm1250,7), strcat('allKCsNoDecay-',datestr(now,'yyyymmddTHHMM'),'.tiff'),[0 8000]);
 
-subtypeNumbers = [607 %g
-    92 %g-d
-    124 %ab-a
-    250 %ab-c
+subtypeNumbers = [590 %g-m
+    8 %g-t
+    99 %g-d
     60 %ab-p
-    452 %ab-s
-    336]; %a'b'
+    223 %ab-s
+    354 %ab-m
+    252 %ab-c
+    91 %a'b'-ap1
+    127 %a'b'-ap2
+    119]; %a'b'-m
 
 % Fig 8J - self vs all KCs
 % Fig 8K - self vs only KCs of the same subtype
@@ -41,8 +44,8 @@ for i=1:length(electroDistConstants)
 end
 
 aplScramble = apl.scrambleUniquePartners([1 2]);
-resultScrambled = aplScramble.weightedSumBetweenSynSets(625, KCtoAPLEDtoAPLtoKC, [2 1],...
-    {{'CA(R)','PED(R)','gL(R)','aL(R)', 'bL(R)', 'a''L(R)', 'b''L(R)'}});
+resultScrambled = aplScramble.weightedSumBetweenSynSets(624, KCtoAPLEDtoAPLtoKC, [2 1],...
+    {apl.synSet(1).synROIskey});
 save('result_KCsScrambled_lambda625.mat','resultScrambled');
 [selfVsAllKCs(:,length(electroDistConstants)+1), ~] = ...
     apl.selfVsOthers(resultScrambled(totalPerm1250,totalPerm1250),'columns',length(totalPerm1250));
@@ -59,15 +62,15 @@ resultCalyx = zeros(numKCs, numKCs, length(electroDistConstants));
 % each loop will take ~15-20'
 for i=1:length(electroDistConstants)
     resultCalyx(:,:,i) = apl.weightedSumBetweenSynSets(electroDistConstants(i), KCtoAPLEDtoAPLtoKC, [2 1],...
-        {'CA(R)'});
+        {{'CA(R)','','SLP(R)','SCL(R)','ICL(R)','PLP(R)'}},1);
 end
 save(strcat('results_calyx_',datestr(now,'yyyymmddTHHMMSS'),'.mat'),'resultCalyx');
 resultCalyxScrambled = aplScramble.weightedSumBetweenSynSets(625, KCtoAPLEDtoAPLtoKC, [2 1],...
-    {'CA(R)'});
+    {{'CA(R)','','SLP(R)','SCL(R)','ICL(R)','PLP(R)'}},1);
 save('result_KCsScrambled_lambda625_calyx.mat','resultCalyxScrambled');
     
 % Fig 8I
-writeTiffParulaWithRange(resultCalyx(totalPerm1250,totalPerm1250,2), 'calyx1250.tiff',[0 200]);
+writeTiffParulaWithRange(resultCalyx(totalPerm1250,totalPerm1250,2), strcat('calyx1250-',datestr(now,'yyyymmddTHHMM'),'.tiff'),[0 200]);
 
 % Fig 8L
 selfVsSameTypeKCsCalyx = zeros(length(totalPerm1250), length(electroDistConstants)+1);
--- runWeightedSumBetweenSynSetsRealDist.m
+++ runWeightedSumBetweenSynSetsRealDist.m
@@ -1,35 +1,38 @@
-electroDistConstants = [625, 1250, 1875, 2381, 5590, 11180, Inf];
-realDistConstants = electroDistConstants*5;
+electroDistConstants = [624, 1249, 1873, 2381, 5590, 11180, Inf];
+realDistConstants = electroDistConstants*5.0062;
 
 % This section contains examples of commands used to generae Fig 8I-L (but
 % not every condition)
 % Fig 8J, K
-numKCs = 1929;
+numKCs = length(apl.synSet(1).uniquePartners);
 
 resultAll = zeros(numKCs, numKCs, length(realDistConstants));
 % eThis takes about 90'
 for i=1:length(realDistConstants)
     resultAll(:,:,i) = apl.weightedSumBetweenSynSets(realDistConstants(i), KCtoAPLrealDistToAPLtoKC, [2 1],...
-        {{'CA(R)','PED(R)','gL(R)','aL(R)', 'bL(R)', 'a''L(R)', 'b''L(R)'}});
+        {apl.synSet(1).synROIskey}); %{'CA(R)','PED(R)','gL(R)','aL(R)', 'bL(R)', 'a''L(R)', 'b''L(R)'}
 end
 save(strcat('results_allKCs_realDist_',datestr(now,'yyyymmddTHHMMSS'),'.mat'),'resultAll');
 
 % the following script is used to sort KCs by subtype:
-[resultsAllreordered, totalPerm6250] = apl.reorderKCs(resultAll(:,:,2)); % use 6250
+[resultsAllreordered, totalPerm6250] = apl.reorderKCsv11(resultAll(:,:,2)); % use 6250
 % save the order of KCs from 50 um, all KCs (Fig 8G)
 
 % Fig 8G
-writeTiffParulaWithRange(resultAll(totalPerm6250,totalPerm6250,2), 'allKCs_realDist.tiff',[0 500]);
+writeTiffParulaWithRange(resultAll(totalPerm6250,totalPerm6250,2), strcat('allKCs_realDist-',datestr(now,'yyyymmddTHHMM'),'.tiff'),[0 500]);
 % Fig 8H
-writeTiffParulaWithRange(resultAll(totalPerm6250,totalPerm6250,6), 'allKCsNoDecay_realDist.tiff',[0 8000]);
+writeTiffParulaWithRange(resultAll(totalPerm6250,totalPerm6250,6), strcat('allKCsNoDecay_realDist-',datestr(now,'yyyymmddTHHMM'),'.tiff'),[0 8000]);
 
-subtypeNumbers = [607 %g
-    92 %g-d
-    124 %ab-a
-    250 %ab-c
+subtypeNumbers = [590 %g-m
+    8 %g-t
+    99 %g-d
     60 %ab-p
-    452 %ab-s
-    336]; %a'b'
+    223 %ab-s
+    354 %ab-m
+    252 %ab-c
+    91 %a'b'-ap1
+    127 %a'b'-ap2
+    119]; %a'b'-m
 
 % Fig 8J - self vs all KCs
 % Fig 8K - self vs only KCs of the same subtype
@@ -42,7 +45,7 @@ end
 
 aplScramble = apl.scrambleUniquePartners([1 2]);
 resultScrambled = aplScramble.weightedSumBetweenSynSets(3125, KCtoAPLrealDistToAPLtoKC, [2 1],...
-    {{'CA(R)','PED(R)','gL(R)','aL(R)', 'bL(R)', 'a''L(R)', 'b''L(R)'}});
+    {apl.synSet(1).synROIskey});
 save('result_KCsScrambled_realDist_lambda3125.mat','resultScrambled');
 [selfVsAllKCs(:,length(realDistConstants)+1), ~] = ...
     apl.selfVsOthers(resultScrambled(totalPerm6250,totalPerm6250),'columns',length(totalPerm6250));
@@ -59,15 +62,15 @@ resultCalyx = zeros(numKCs, numKCs, length(realDistConstants));
 % each loop will take ~15-20'
 for i=1:length(realDistConstants)
     resultCalyx(:,:,i) = apl.weightedSumBetweenSynSets(realDistConstants(i), KCtoAPLrealDistToAPLtoKC, [2 1],...
-        {'CA(R)'});
+        {{'CA(R)','','SLP(R)','SCL(R)','ICL(R)','PLP(R)'}},1);
 end
 save(strcat('results_calyx_',datestr(now,'yyyymmddTHHMMSS'),'.mat'),'resultCalyx');
 resultCalyxScrambled = aplScramble.weightedSumBetweenSynSets(3125, KCtoAPLrealDistToAPLtoKC, [2 1],...
-    {'CA(R)'});
+    {{'CA(R)','','SLP(R)','SCL(R)','ICL(R)','PLP(R)'}},1);
 save('result_KCsScrambled_realDist_lambda3125_calyx','resultCalyxScrambled');
     
 % Fig 8I
-writeTiffParulaWithRange(resultCalyx(totalPerm6250,totalPerm6250,2), 'calyx6250_realDist.tiff',[0 200]);
+writeTiffParulaWithRange(resultCalyx(totalPerm6250,totalPerm6250,2), strcat('calyx6250_realDist-',datestr(now,'yyyymmddTHHMM'),'.tiff'),[0 200]);
 
 % Fig 8L
 selfVsSameTypeKCsCalyx = zeros(length(totalPerm6250), length(realDistConstants)+1);
