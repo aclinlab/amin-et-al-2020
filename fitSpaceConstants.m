8ffbb6a2230d07540a6448bfdfb286fad1e28bda
Andrew Lin <andrewclin@gmail.com>
2020-07-25 22:36:28 +0100

    Update fitSpaceConstants.m
    update for new ratio of k vs lambda in v1.1

--- fitSpaceConstants.m
+++ fitSpaceConstants.m
@@ -6,11 +6,11 @@ redDyeFits = [hstimFit, vstimFit, cstimFit];
 outputNames = {'hstim', 'vstim', 'cstim'};
 % these are values of k in units of sqrt(pixels)
 % to convert to sqrt(m), multiply by sqrt(8e-9 m/pixel) (8 nm = 1 pixel)
-electroDistConstants = [625, 1250, 1875, 2381, 11180]; % k = .0559, 0.1118, 0.1677, 0.213, 1.0 m
+electroDistConstants = [624, 1249, 1873, 2381, 11180]; % k = .0559, 0.1118, 0.1677, 0.213, 1.0 m
 % the value k=0.213 m comes from Cuntz et al 2013 where Rm = 0.8166 Ohm*m^2
 % and Ra = 9 Ohm*m. k=1.0 m comes from Gouwens et al 2009, Rm = 2.04 Ohm*m^2
 % and Ra = 1.02 Ohm*m.
-realDistConstants = electroDistConstants*5; %this gives real distance space constant in pixels
+realDistConstants = electroDistConstants*5.0062; %this gives real distance space constant in pixels
 % to convert to m, multiply by 8e-9 m/pixel
 % 25, 50, 75, 95.2, 223.6, 447.2 um
 micronConstants = realDistConstants*8e-9/1e-6; % space constants in microns
